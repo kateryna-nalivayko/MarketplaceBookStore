8875e4d8d27738141d3ec421d4b1c35ad0c3fd8e
Kateryna Nalyvaiko <nalyvakokate@mail.com>
2025-02-06 21:07:22 +0200

    Add customer profile functionality: implement customer profile view, update URLs, and create customer dashboard template

--- customer/urls.py
+++ customer/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-from users import views
+from customer import views
 
 app_name='customer'
 
 urlpatterns = [
-    
+    path('profile/', views.customer_profile, name='customer_profile')
 ]
--- customer/views.py
+++ customer/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+from customer.models import Customer
+
+
+@login_required
+def customer_profile(request):
+    if request.user.groups.filter(name='Customer').exists():
+        customer = get_object_or_404(Customer, user=request.user)
+
+
+        context = {
+            'customer': customer,
+            'user': request.user
+        }
+
+        return render(request, 'customer/customer-profile.html', context)
+
